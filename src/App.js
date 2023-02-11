684937e7351fda771e23d2415054a4b4b6fad904
yoshichiban <jdcstarita@gmail.com>
2023-02-11 12:31:03 +0800

    removed comments

--- src/App.js
+++ src/App.js
@@ -3,17 +3,6 @@ import './App.css';
 
 const App = () => {
   const [counter, setCounter] = useState(0);
-/*
-Whenever you call something as a function and it starts with "use", in React, we call that a hook.
-const [counter, setCounter] = useState(0);
-
-inside brackets
-1. name of that state(counter)
-2. setter function(setCounter) - rule of thumb to add "set" to the name
-provide initial value on parenthesis
-
-Now we can use this counter as a normal JS variable
-*/
 
   return (
     <div className="App">
