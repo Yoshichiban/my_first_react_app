5f66b38771efd59d67741873fe70dc852e46dbcc
yoshichiban <jdcstarita@gmail.com>
2023-02-11 13:33:04 +0800

    Added MovieCard Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import { useEffect } from "react";
 
+import MovieCard from './MovieCard';
+
 import './App.css';
 import SearchIcon from './search.svg';
 
 const API_URL = 'http://www.omdbapi.com?apikey=459a3afa';
+
+const movie1 = 
+    {
+        "Title": "Superman, Spiderman or Batman",
+        "Year": "2011",
+        "imdbID": "tt2084949",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ4MzcxNDU3N15BMl5BanBnXkFtZTgwOTE1MzMxNzE@._V1_SX300.jpg"
+    };
 
 const App = () => {
     const searchMovies = async (title) => {
@@ -18,7 +29,28 @@ const App = () => {
     }, []); 
 
     return(
-        <h1>App</h1>
+        <div className='app'>
+            <h1> MovieLand </h1>
+
+            <div className='search'>
+                <input
+                    placeholder='Search for movies'
+                    value='Superman'
+                    onChange={() => {}}
+                />
+                <img 
+                    src={SearchIcon}
+                    alt='search'
+                    onClick={() => {}}
+                >
+                </img>
+            </div>{/* end of search */}
+
+            <div className='container'>
+                <MovieCard movie1={movie1}/>
+            </div>{/* end of container */}
+
+        </div>
     );
 }
 
